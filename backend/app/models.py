b950a1473020f09d31f3ca356e6456d3f1f60500
cicerojr10 <cicerojr10@gmail.com>
2026-01-21 22:34:44 -0300

    feat: adicionando tabela de usuarios

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,20 +1,31 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from .database import Base
 
-# 1. Nova Tabela: Categorias
+# --- TABELA DE USUÁRIOS (NOVA) ---
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True) # Email não pode repetir
+    hashed_password = Column(String) # Senha criptografada (nunca salvamos a senha real)
+
+    # Relacionamento: Um usuário tem várias transações
+    transactions = relationship("Transaction", back_populates="owner")
+
+# --- TABELA DE CATEGORIAS ---
 class Category(Base):
     __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True) # Ex: Alimentação
-    icon = Column(String)  # Ex: "utensils" (nome do ícone)
-    color = Column(String) # Ex: "#ef4444" (cor vermelha)
-
+    name = Column(String, unique=True, index=True)
+    icon = Column(String)
+    color = Column(String)
+    
     # Relacionamento: Uma categoria tem várias transações
     transactions = relationship("Transaction", back_populates="category")
 
-# 2. Tabela Atualizada: Transações
+# --- TABELA DE TRANSAÇÕES ---
 class Transaction(Base):
     __tablename__ = "transactions"
 
@@ -23,10 +34,11 @@ class Transaction(Base):
     amount = Column(Float)
     type = Column(String) # 'income' ou 'expense'
     date = Column(DateTime)
-    user_id = Column(Integer)
 
-    # NOVO: Link com a Categoria
+    # Conexão com Usuário (Mudou aqui!)
+    user_id = Column(Integer, ForeignKey("users.id")) 
+    owner = relationship("User", back_populates="transactions")
+
+    # Conexão com Categoria
     category_id = Column(Integer, ForeignKey("categories.id"), nullable=True)
-    
-    # Relacionamento: Uma transação pertence a uma categoria
     category = relationship("Category", back_populates="transactions")
