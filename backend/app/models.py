90f898fc5035e2dde9f6cda53b95d3e6584a9cec
cicerojr10 <cicerojr10@gmail.com>
2026-01-29 22:18:56 -0300

    fix: limpando models para corrigir erro de foreign key

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,31 +1,32 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, Boolean
+from sqlalchemy import Boolean, Column, Integer, String, Float, DateTime, ForeignKey, func
 from sqlalchemy.orm import relationship
 from .database import Base
 
-# --- TABELA DE USUÁRIOS (NOVA) ---
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True) # Email não pode repetir
-    hashed_password = Column(String) # Senha criptografada (nunca salvamos a senha real)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
 
-    # Relacionamento: Um usuário tem várias transações
+    # Relacionamentos
     transactions = relationship("Transaction", back_populates="owner")
+    categories = relationship("Category", back_populates="owner")
 
-# --- TABELA DE CATEGORIAS ---
 class Category(Base):
     __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
+    name = Column(String, index=True)
     icon = Column(String)
     color = Column(String)
     
-    # Relacionamento: Uma categoria tem várias transações
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    
+    # Relacionamentos
+    owner = relationship("User", back_populates="categories")
     transactions = relationship("Transaction", back_populates="category")
 
-# --- TABELA DE TRANSAÇÕES ---
 class Transaction(Base):
     __tablename__ = "transactions"
 
@@ -33,20 +34,12 @@ class Transaction(Base):
     description = Column(String, index=True)
     amount = Column(Float)
     type = Column(String) # 'income' ou 'expense'
-    date = Column(DateTime)
-
-    # Conexão com Usuário (Mudou aqui!)
-    user_id = Column(Integer, ForeignKey("users.id")) 
-    owner = relationship("User", back_populates="transactions")
-
-    # Conexão com Categoria
-    category_id = Column(Integer, ForeignKey("categories.id"), nullable=True)
-    category = relationship("Category", back_populates="transactions")
-
-    is_fixed = Column(Boolean, default=False) 
+    date = Column(DateTime(timezone=True), server_default=func.now())
+    is_fixed = Column(Boolean, default=False) # Nossa coluna nova aqui!
 
     category_id = Column(Integer, ForeignKey("categories.id"))
     owner_id = Column(Integer, ForeignKey("users.id"))
     
+    # Relacionamentos
     category = relationship("Category", back_populates="transactions")
     owner = relationship("User", back_populates="transactions")
